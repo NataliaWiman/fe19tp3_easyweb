3edee4ccd99ed840728a1346dee59cbcc54da5ed
annalysethis <annalys01@gmail.com>
2020-05-27 14:51:25 +0200

    fixed about page

--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -1,37 +1,6 @@
 import React from "react";
-import styled from "styled-components";
+import * as skill from "./Skills.styled";
 import Skill from "./Progress";
-import * as Styled from "../../styles/styled";
-
-const SkillsWrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Content = styled.div`
-  text-align: center;
-  max-width: 25%;
-  margin-bottom: 100px;
-  h2 {
-    font-family: "Montserrat", sans-serif;
-    font-weight: 400;
-    font-size: 30px;
-    text-transform: uppercase;
-    margin-top: 100px;
-  }
-  p {
-    margin-top: 50px;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 1.667;
-  }
-`;
-
-const Heading = styled(Styled.PageTitle)`
-  font-size: ${({ theme }) => theme.fontSize};
-  text-align: center;
-`;
 
 const skillList = [
   {
@@ -58,19 +27,19 @@ const skillList = [
 
 const Skills = () => {
   return (
-    <SkillsWrapper id="skills">
-      <Content>
-        <Heading>Expertis Skills</Heading>
+    <skill.SkillsWrapper id="skills">
+      <skill.Content>
+        <skill.Heading>Expertis Skills</skill.Heading>
         <p>
           Dui aliquam quisque. Nec feugiat ut neque odio quo, proin interdum,
           quis elit varius et duis, dui augue vehicula nam tinc.
         </p>
-      </Content>
+      </skill.Content>
 
       {skillList.map(({ name, value }, index) => (
         <Skill skill={name} percent={value} key={index} />
       ))}
-    </SkillsWrapper>
+    </skill.SkillsWrapper>
   );
 };
 
